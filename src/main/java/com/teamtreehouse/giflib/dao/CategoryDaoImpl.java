8bb9ce04198ccc54af72253247ab3c28a19579fd
Ryan Wittrup <ryan.wittrup@gmail.com>
2018-02-21 08:06:24 -0600

    implements dao and service for category entity

---
+++ src/main/java/com/teamtreehouse/giflib/dao/CategoryDaoImpl.java
@@ -0,0 +1,47 @@
+package com.teamtreehouse.giflib.dao;
+
+import com.teamtreehouse.giflib.model.Category;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
+@Repository
+public class CategoryDaoImpl implements CategoryDao{
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Category> findAll() {
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Category> q = cb.createQuery(Category.class);
+        q.from(Category.class);
+
+        List<Category> categories = session.createQuery(q).getResultList();
+
+        session.close();
+
+        return categories;
+    }
+
+    @Override
+    public Category findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Category category) {
+
+    }
+
+    @Override
+    public void delete(Category category) {
+
+    }
+}
