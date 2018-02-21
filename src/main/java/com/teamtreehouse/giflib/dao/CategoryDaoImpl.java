a495eb70e5eecd7b02a148cc5e7def35382c34eb
Ryan Wittrup <ryan.wittrup@gmail.com>
2018-02-21 08:53:08 -0600

    adds dao and service logic for creating category, updates form template to support post request

--- src/main/java/com/teamtreehouse/giflib/dao/CategoryDaoImpl.java
+++ src/main/java/com/teamtreehouse/giflib/dao/CategoryDaoImpl.java
@@ -37,7 +37,13 @@ public class CategoryDaoImpl implements CategoryDao{
 
     @Override
     public void save(Category category) {
+        Session session = sessionFactory.openSession();
 
+        session.beginTransaction();
+        session.save(category);
+        session.getTransaction().commit();
+
+        session.close();
     }
 
     @Override
